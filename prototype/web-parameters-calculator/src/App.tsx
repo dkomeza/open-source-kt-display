4d00afaf6957d807bcc15238ee2eb454e81e635f
Dawid Komęza <dawid.komeza@outlook.com>
2022-09-15 10:24:14 +0000

    Form testing

--- prototype/web-parameters-calculator/src/App.tsx
+++ prototype/web-parameters-calculator/src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import './scss/style.scss'
 
 function App() {
+  const [code, setCode] = useState([98, 123, 213])
+  const b12Ref = useRef<HTMLInputElement>(null);
+  
+  function handleInput() {
+    setCode(code => [...code, Number(b12Ref.current?.value)])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        Open Source KT Displaya
+      <header>
+        <h1>Open Source KT Display</h1>
       </header>
+      <main>
+        <section>
+          <form onInput={handleInput}>
+            <input type={"number"} ref={b12Ref}></input>
+          </form>
+          <div>{ code.join(" ") }</div>
+        </section>
+      </main>
     </div>
   );
 }
