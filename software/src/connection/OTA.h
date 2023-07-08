6966f4b43863971f0a1005e5501e1a3ac6d5583e
Dawid Komęza <dawid.komeza@gmail.com>
2023-07-08 12:09:35 +0200

    :sparkles: Add `setupOTA` function

--- software/src/connection/OTA.h
+++ software/src/connection/OTA.h
@@ -2,7 +2,25 @@
 #define OSKD_OTA_H
 
 #include <Arduino.h>
+#include <ArduinoOTA.h>
+#include <ESPmDNS.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
 
 #include "./credentials.h" // ssid and password
+
+IPAddress setupOTA()
+{
+    ArduinoOTA.setHostname("OSKD");
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    WiFi.waitForConnectResult(5000);
+
+    ArduinoOTA.begin();
+
+    return WiFi.localIP();
+}
 
 #endif // OSKD_OTA_H
