26af1a62fdfa41bb53747feb0535e0b8dcb69df3
Dawid Komęza <dawid.komeza@gmail.com>
2023-07-08 12:05:14 +0200

    :heavy_plus_sign: Add includes in main file

--- software/src/open-source-kt-display.cpp
+++ software/src/open-source-kt-display.cpp
@@ -1,9 +1,18 @@
+#include <Arduino.h>
+
+#include "connection/OTA.h"
+#include "controller/controller.h"
+#include "data/data.h"
+#include "io/io.h"
+#include "screen/screen.h"
+#include "settings/settings.h"
+
+Controller controller;
+
 void setup()
 {
-
 }
 
 void loop()
 {
-
 }
