d01ad694f71ced716181235b7829447e3d2ac3eb
Dawid Komęza <dawid.komeza@gmail.com>
2023-07-08 11:51:15 +0200

    :sparkles: Add `controller` and `OTA` files

---
+++ software/src/connection/OTA.h
@@ -0,0 +1,8 @@
+#ifndef OSKD_OTA_H
+#define OSKD_OTA_H
+
+#include <Arduino.h>
+
+#include "./credentials.h"
+
+#endif // OSKD_OTA_H
---
+++ software/src/controller/controller.h
@@ -0,0 +1,12 @@
+#ifndef OSKD_CONTROLLER_H
+#define OSKD_CONTROLLER_H
+
+#include <Arduino.h>
+
+class Controller
+{
+};
+
+extern Controller controller;
+
+#endif // OSKD_CONTROLLER_H
