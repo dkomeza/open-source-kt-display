8fb3ab9d8cffad8004b851d24c1f308dcec758bf
Dawid Komęza <dawid.komeza@gmail.com>
2023-07-08 18:03:05 +0200

    :zap: General update

--- software/src/open-source-kt-display.cpp
+++ software/src/open-source-kt-display.cpp
@@ -13,14 +13,21 @@ IO io;
 Screen screen;
 Settings settings;
 
+long lastUpdate = 0;
+
 void setup()
 {
+    pinMode(26, OUTPUT);
+    dacWrite(26, 200);
+
     setupOTA();
 
     controller.setup();
     screen.setup();
     io.setup();
     settings.setup();
+
+    lastUpdate = millis();
 }
 
 void loop()
@@ -30,4 +37,9 @@ void loop()
     io.update();
 
     ArduinoOTA.handle();
+
+    if (millis() - lastUpdate < 50)
+    {
+        delay(50 - (millis() - lastUpdate));
+    }
 }
