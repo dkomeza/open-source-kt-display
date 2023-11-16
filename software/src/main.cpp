9e30a0c6f4ee3c5d10b5fe70adfa9861b97b7114
Dawid Komęza <dawid.komeza@gmail.com>
2023-11-16 17:22:01 +0100

    :sparkles: Add basic main loop

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,3 +1,21 @@
-void setup() {}
+#include <Arduino.h>
 
-void loop() {}
+long lastTime = 0;
+
+void setup()
+{
+    lastTime = millis();
+}
+
+void loop()
+{
+
+    // Consistent loop time (10ms)
+    long currentTime = millis();
+    int timeDiff = currentTime - lastTime;
+    if (timeDiff < 10)
+    {
+        delay(10 - timeDiff);
+    }
+    lastTime = millis();
+}
