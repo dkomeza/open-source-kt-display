352ef94266244f8c79771ac404eb1ff586b47137
Dawid Komęza <dawid.komeza@gmail.com>
2023-11-17 20:18:43 +0100

    :sparkles: Add buttons to the main loop

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,19 +1,22 @@
 #include <Arduino.h>
 
 #include "connection/OTA.h"
+#include "button/buttons.h"
 
 long lastTime = 0;
 
 void setup()
 {
-    lastTime = millis();
-
     setupOTA();
+    Buttons::setup();
+
+    lastTime = millis();
 }
 
 void loop()
 {
     ArduinoOTA.handle();
+    Buttons::update();
 
     // Consistent loop time (10ms)
     long currentTime = millis();
